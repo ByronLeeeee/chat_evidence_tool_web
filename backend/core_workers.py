b84a8311bfa52077ebb624b527f0a86b54271eb1
ByronLeeeee <demonbyron@gmail.com>
2025-06-10 12:40:13 +0800

    fix(core_workers): 更新PaddleOCR初始化参数以启用GPU支持并禁用日志显示 fix(requirements): 更新paddleocr依赖版本限制，一切的bug都来源于paddleocr 3.0，现在限制在<3.0.0的状态

--- backend/core_workers.py
+++ backend/core_workers.py
@@ -34,7 +34,7 @@ OCR_ENGINE = None
 try:
     print("正在初始化 PaddleOCR 引擎...")
     # 如果需要，可以考虑添加更具体的模型路径，或通过环境变量控制
-    OCR_ENGINE = PaddleOCR()
+    OCR_ENGINE = PaddleOCR(use_angle_cls=True, show_log=False, use_gpu=True,)
     print("✅ PaddleOCR 引擎初始化成功。")
 except ImportError:
     print("⚠️ 错误: 未找到 paddleocr 或 paddlepaddle 库。OCR 功能将被禁用。")
@@ -543,7 +543,7 @@ class OcrFilter:
 
                     # --- 执行 OCR ---
                     ocr_results = self.ocr_engine.ocr(
-                        str(path_for_ocr))
+                        str(path_for_ocr),cls=True)
 
                     current_raw_lines = []  # 当前帧的原始OCR行
                     if ocr_results and ocr_results[0]:  # 检查结果是否有效
